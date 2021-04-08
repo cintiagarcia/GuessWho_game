fdab4c501d0a47d5deec1783254daad516b47723
Cintia Garcia <cintiagarcia@air-de-cintia.lan>
2021-04-08 17:32:24 +0200

    fix relative path

--- js/index.js
+++ js/index.js
@@ -1,195 +1,193 @@
-const cards = [{
-        name: "alien",
-        img: "alien.png",
-        features: ["green"],
-    },
-    {
-        name: "batman",
-        img: "batman.png",
-        features: ["cap", "human", "mask"],
-    },
-    {
-        name: "baymax",
-        img: "baymax.png",
-        features: ["marvel"],
-    },
-    {
-        name: "branger",
-        img: "branger.png",
-        features: ["powerRanger", "helmet", "glasses", "human"],
-    },
-    {
-        name: "chicken",
-        img: "chicken.png",
-        features: ["glasses"],
-    },
-    {
-        name: "cyclops",
-        img: "cyclops.png",
-        features: ["x-men", "marvel", "human", "glasses"],
-    },
-    {
-        name: "flash",
-        img: "flash.png",
-        features: ["marvel", "helmet", "red", "human"],
-    },
-    {
-        name: "granger",
-        img: "granger.png",
-        features: ["powerRanger", "helmet", "glasses", "green", "human"],
-    },
-    {
-        name: "hulk",
-        img: "hulk.png",
-        features: ["green", "human", "marvel"],
-    },
-    {
-        name: "leonidas",
-        img: "leonidas.png",
-        features: ["human", "mustache", "weapon"],
-    },
-    {
-        name: "luigi",
-        img: "luigi.png",
-        features: ["mustache"],
-    },
-    {
-        name: "mario",
-        img: "mario.png",
-        features: ["mustache"],
-    },
-    {
-        name: "magneto",
-        img: "magneto.png",
-        features: ["x-men", "marvel", "red", "human", "cap", "helmet"],
-    },
-    {
-        name: "pranger",
-        img: "pranger.png",
-        features: ["powerRanger", "helmet", "glasses", "human"],
-    },
-    {
-        name: "robocop",
-        img: "robocop.png",
-        features: ["weapon", "helmet"],
-    },
-    {
-        name: "rranger",
-        img: "rranger.png",
-        features: ["powerRanger", "helmet", "glasses", "red", "human"],
-    },
-    {
-        name: "superman",
-        img: "superman.png",
-        features: ["cap", "human"],
-    },
-    {
-        name: "thor",
-        img: "thor.png",
-        features: ["weapon", "helmet", "cap", "marvel"],
-    },
-    {
-        name: "wolverine",
-        img: "wolverine.png",
-        features: ["x-men", "marvel", "mask", "human"],
-    },
-    {
-        name: "yranger",
-        img: "yranger.png",
-        features: ["powerRanger", "helmet", "glasses", "human"],
-    },
+const cards = [
+  {
+    name: "alien",
+    img: "alien.png",
+    features: ["green"],
+  },
+  {
+    name: "batman",
+    img: "batman.png",
+    features: ["cap", "human", "mask"],
+  },
+  {
+    name: "baymax",
+    img: "baymax.png",
+    features: ["marvel"],
+  },
+  {
+    name: "branger",
+    img: "branger.png",
+    features: ["powerRanger", "helmet", "glasses", "human"],
+  },
+  {
+    name: "chicken",
+    img: "chicken.png",
+    features: ["glasses"],
+  },
+  {
+    name: "cyclops",
+    img: "cyclops.png",
+    features: ["x-men", "marvel", "human", "glasses"],
+  },
+  {
+    name: "flash",
+    img: "flash.png",
+    features: ["marvel", "helmet", "red", "human"],
+  },
+  {
+    name: "granger",
+    img: "granger.png",
+    features: ["powerRanger", "helmet", "glasses", "green", "human"],
+  },
+  {
+    name: "hulk",
+    img: "hulk.png",
+    features: ["green", "human", "marvel"],
+  },
+  {
+    name: "leonidas",
+    img: "leonidas.png",
+    features: ["human", "mustache", "weapon"],
+  },
+  {
+    name: "luigi",
+    img: "luigi.png",
+    features: ["mustache"],
+  },
+  {
+    name: "mario",
+    img: "mario.png",
+    features: ["mustache"],
+  },
+  {
+    name: "magneto",
+    img: "magneto.png",
+    features: ["x-men", "marvel", "red", "human", "cap", "helmet"],
+  },
+  {
+    name: "pranger",
+    img: "pranger.png",
+    features: ["powerRanger", "helmet", "glasses", "human"],
+  },
+  {
+    name: "robocop",
+    img: "robocop.png",
+    features: ["weapon", "helmet"],
+  },
+  {
+    name: "rranger",
+    img: "rranger.png",
+    features: ["powerRanger", "helmet", "glasses", "red", "human"],
+  },
+  {
+    name: "superman",
+    img: "superman.png",
+    features: ["cap", "human"],
+  },
+  {
+    name: "thor",
+    img: "thor.png",
+    features: ["weapon", "helmet", "cap", "marvel"],
+  },
+  {
+    name: "wolverine",
+    img: "wolverine.png",
+    features: ["x-men", "marvel", "mask", "human"],
+  },
+  {
+    name: "yranger",
+    img: "yranger.png",
+    features: ["powerRanger", "helmet", "glasses", "human"],
+  },
 ];
 
 const guessWho = new GuessWho(cards);
 
 window.addEventListener("load", (event) => {
-    createHtmlDiv();
+  createHtmlDiv();
 
-    document.querySelector(".combo-box").innerHTML = guessWho.generateQuestions();
+  document.querySelector(".combo-box").innerHTML = guessWho.generateQuestions();
 
-    // Bind the click event of each element to a function
-    clickEvent();
-
+  // Bind the click event of each element to a function
+  clickEvent();
 });
 
 function askButtonPressed() {
-    console.log(`Push Button Ask`);
-    var comboBoxValue = document.querySelector(".select-css").value;
+  console.log(`Push Button Ask`);
+  var comboBoxValue = document.querySelector(".select-css").value;
 
-    console.log(guessWho.questions[comboBoxValue].features);
+  console.log(guessWho.questions[comboBoxValue].features);
 
-    var featuresSelected = guessWho.questions[comboBoxValue].features;
-    guessWho.score -= 500;
-    console.log(guessWho.score);
-    // delete the element with this index
-    
-    guessWho.questions.splice(comboBoxValue, 1);
-    document.querySelector(".combo-box").innerHTML = guessWho.generateQuestions();
-    document.querySelector("#points").innerHTML = guessWho.score;
-    checkScore();
-    let selectedCardContainsFeature = guessWho.selectedCard.features.includes(
-        featuresSelected
-    );
+  var featuresSelected = guessWho.questions[comboBoxValue].features;
+  guessWho.score -= 500;
+  console.log(guessWho.score);
+  // delete the element with this index
 
-    // filter cards with the following condition: if the feature property is or not inside the card
+  guessWho.questions.splice(comboBoxValue, 1);
+  document.querySelector(".combo-box").innerHTML = guessWho.generateQuestions();
+  document.querySelector("#points").innerHTML = guessWho.score;
+  checkScore();
+  let selectedCardContainsFeature = guessWho.selectedCard.features.includes(
+    featuresSelected
+  );
 
-    guessWho.cards = guessWho.cards.filter(function (card) {
-        if (selectedCardContainsFeature) {
-            return card.features.includes(featuresSelected);
-        } else {
-            return !card.features.includes(featuresSelected);
-        }
-    });
+  // filter cards with the following condition: if the feature property is or not inside the card
 
-    //create html div
-    createHtmlDiv();
+  guessWho.cards = guessWho.cards.filter(function (card) {
+    if (selectedCardContainsFeature) {
+      return card.features.includes(featuresSelected);
+    } else {
+      return !card.features.includes(featuresSelected);
+    }
+  });
 
-    // Bind the click event of each element to a function
-    clickEvent();
+  //create html div
+  createHtmlDiv();
+
+  // Bind the click event of each element to a function
+  clickEvent();
 }
 
 var currentCard = " ";
 
 function checkScore() {
-    if (guessWho.score == 0) {
-        window.location.href = "/game-over.html";
-    }
+  if (guessWho.score == 0) {
+    window.location.href = "game-over.html";
+  }
 }
 
 function clickEvent() {
-    document.querySelectorAll(".card").forEach((card) => {
-        card.addEventListener("click", () => {
-            console.log(`card.getAttribute("card-name")`);
+  document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("click", () => {
+      console.log(`card.getAttribute("card-name")`);
 
-            if (guessWho.selectedCard.name === card.getAttribute("card-name")) {
-                alert("YOU WIN!!");
-                console.log(`You win`);
-            } else {
-                console.log(`You are wrong`);
-                alert("YOU ARE WRONG!!LET TRY AGAIN!!");
-                if (guessWho.score >= 400) {
-                    guessWho.score -= 400;
-                } else {
-                    guessWho.score = 0;
-                }
-                checkScore();
-                document.querySelector("#points").innerHTML = guessWho.score;
-            }
-        });
+      if (guessWho.selectedCard.name === card.getAttribute("card-name")) {
+        alert("YOU WIN!!");
+        console.log(`You win`);
+      } else {
+        console.log(`You are wrong`);
+        alert("YOU ARE WRONG!!LET TRY AGAIN!!");
+        if (guessWho.score >= 400) {
+          guessWho.score -= 400;
+        } else {
+          guessWho.score = 0;
+        }
+        checkScore();
+        document.querySelector("#points").innerHTML = guessWho.score;
+      }
     });
+  });
 }
 
 function createHtmlDiv() {
-    let html = "";
-    guessWho.cards.forEach((pic) => {
-        console.log(`image loaded: ${pic.img}`);
-        html += `<div class="card" card-name="${pic.name}">`;
-        html += `<div class="front" style="background: url(img/${pic.img}) no-repeat; background-size: contain"></div>`;
-        html += `</div>`;
-    });
+  let html = "";
+  guessWho.cards.forEach((pic) => {
+    console.log(`image loaded: ${pic.img}`);
+    html += `<div class="card" card-name="${pic.name}">`;
+    html += `<div class="front" style="background: url(img/${pic.img}) no-repeat; background-size: contain"></div>`;
+    html += `</div>`;
+  });
 
-    // Add all the divs to the HTML
-    document.querySelector("#guessWho-board").innerHTML = html;
+  // Add all the divs to the HTML
+  document.querySelector("#guessWho-board").innerHTML = html;
 }
-
-
